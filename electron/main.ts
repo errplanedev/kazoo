dd41b2d2444d353bca79f04ebad7ff4d6fb61e5a
errplanedev <110797323+errplanedev@users.noreply.github.com>
2026-01-13 18:54:17 -0800

    TYPESCRIPT WHY

--- electron/main.ts
+++ electron/main.ts
@@ -3,7 +3,6 @@ import { createRequire } from "node:module";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
 
-const require = createRequire(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // The built directory structure
